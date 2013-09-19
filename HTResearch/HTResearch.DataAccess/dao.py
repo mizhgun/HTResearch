eb7b9293263e5ae33030f38a55c08519c0826008
Jordan Degner <jdegner0129@gmail.com>
2013-09-18 21:03:07 -0500

    Created a DAOFactory and tested using the db_connect.py script.

--- HTResearch/HTResearch.DataAccess/dao.py
+++ HTResearch/HTResearch.DataAccess/dao.py
@@ -1,64 +1,52 @@
 from dto import *
 
-class ContactDAO(Object):
+class ContactDAO(object):
     """A DAO for the Contact document"""
 
-    @classmethod
-    def Create(cls, contact_dto):
+    def Create(self, contact_dto):
         contact_dto.save()
 
-    @classmethod
-    def Delete(cls, contact_dto):
+    def Delete(self, contact_dto):
         contact_dto.delete()
 
-    @classmethod
-    def Edit(cls, contact_dto):
+    def Edit(self, contact_dto):
         old_dto = ContactDTO.objects(id=contact_dto.id).first()
         old_dto = contact_dto
         old_dto.save()
-    
-    @classmethod
-    def Find(cls, dto_id):
+
+    def Find(self, dto_id):
         return ContactDTO.objects(id=dto_id).first()
 
-class OrganizationDAO(Object):
+class OrganizationDAO(object):
     """A DAO for the Organization document"""
 
-    @classmethod
-    def Create(cls, org_dto):
+    def Create(self, org_dto):
         org_dto.save()
 
-    @classmethod
-    def Delete(cls, org_dto):
+    def Delete(self, org_dto):
         org_dto.delete()
 
-    @classmethod
-    def Edit(cls, org_dto):
+    def Edit(self, org_dto):
         old_dto = OrganizationDTO.objects(id=org_dto.id).first()
         old_dto = org_dto
         old_dto.save()
-    
-    @classmethod
-    def Find(cls, dto_id):
+
+    def Find(self, dto_id):
         return OrganizationDTO.objects(id=dto_id).first()
 
-class PublicationDAO(Object):
+class PublicationDAO(object):
     """A DAO for the Publication document"""
 
-    @classmethod
-    def Create(cls, pub_dto):
+    def Create(self, pub_dto):
         pub_dto.save()
 
-    @classmethod
-    def Delete(cls, pub_dto):
+    def Delete(self, pub_dto):
         pub_dto.delete()
 
-    @classmethod
-    def Edit(cls, pub_dto):
+    def Edit(self, pub_dto):
         old_dto = PublicationDTO.objects(id=pub_dto.id).first()
         old_dto = pub_dto
         old_dto.save()
-    
-    @classmethod
-    def Find(cls, dto_id):
+
+    def Find(self, dto_id):
         return PublicationDTO.objects(id=dto_id).first()
---
+++ HTResearch/HTResearch.DataAccess/factory.py
@@ -0,0 +1,13 @@
+from dao import *
+
+class DAOFactory(object):
+    """A Factory class for different DAOs."""
+
+    @staticmethod
+    def GetInstance(cls):
+        if cls.__name__ == "ContactDAO":
+            return ContactDAO()
+        elif cls.__name__ == "OrganizationDAO":
+            return OrganizationDAO()
+        elif cls.__name__ == "PublicationDAO":
+            return PublicationDAO()
