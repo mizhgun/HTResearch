da1d70ba4a055e39f93c4b1f956ac283bc41d61c
Cameron Crockrom <ccrockrom@gmail.com>
2013-09-24 16:52:12 -0500

    Added successful unit tests for database interaction.

--- HTResearch/Test/db_interaction_test.py
+++ HTResearch/Test/db_interaction_test.py
@@ -1,4 +1,10 @@
-from DataAccess import dto
+import sys
+[sys.path.remove(p) for p in sys.path if '../DataAccess/' in p]
+if 'DataAccess' not in sys.path:
+    sys.path.append('../DataAccess/')
+from dto import ContactDTO, OrganizationDTO, PublicationDTO
+from dao import ContactDAO, OrganizationDAO, PublicationDAO
+from factory import DAOFactory
 
 class DatabaseInteractionTestSuite():
 
@@ -28,7 +34,37 @@ class DatabaseInteractionTestSuite():
         my_contact.last_name = "Degner"
         my_contact.email = "jdegner0129@gmail.com"
         DAOFactory.GetInstance(ContactDAO).Create(my_contact)
+        assert_contact=DAOFactory.GetInstance(ContactDAO).Find(my_contact.id)
+        assert assert_contact.first_name == "Jordan"
+        assert assert_contact.last_name == "Degner"
+        assert assert_contact.email == "jdegner0129@gmail.com"
+        print 'ContactDAO tests passed'
+
+        my_organization = OrganizationDTO()
+        my_organization.name = "Yee University"
+        my_organization.organization_url = "http://com.bee.yee"
+        contacts = []
+        contacts.append(my_contact)
+        contacts.append(assert_contact)
+        my_organization.contacts = contacts
+        DAOFactory.GetInstance(OrganizationDAO).Create(my_organization)
+        assert_organization=DAOFactory.GetInstance(OrganizationDAO).Find(my_organization.id)
+        assert assert_organization.contacts == contacts
+        assert assert_organization.name == my_organization.name
+        assert assert_organization.organization_url == my_organization.organization_url
+        print 'OrganizationDAO tests passed'
+
+        my_publication = PublicationDTO()
+        my_publication.title = "The Book of Yee"
+        my_publication.authors = contacts
+        my_publication.publication_date = '2006-10-25 14:30:59'
+        DAOFactory.GetInstance(PublicationDAO).Create(my_publication)
+        assert_publication=DAOFactory.GetInstance(PublicationDAO).Find(my_publication.id)
+        assert assert_publication.title == my_publication.title
+        assert assert_publication.authors == my_publication.authors
+        print 'PublicationDAO tests passed'
         self.destroy_database()
 
 if __name__:
-    my_suite = DatabaseAccessTestSuite()
+    my_suite = DatabaseInteractionTestSuite()
+    print 'DB interaction test complete'
