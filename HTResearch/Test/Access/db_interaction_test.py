8daacc4a639bd3bcccc199cf06e5bb315b289444
Cameron Crockrom <ccrockrom@gmail.com>
2013-09-24 20:28:56 -0500

    Started using unittest framework.

--- HTResearch/Test/Access/db_interaction_test.py
+++ HTResearch/Test/Access/db_interaction_test.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 [sys.path.remove(p) for p in sys.path if 'DataAccess' in p]
 sys.path.append('../DataAccess/')
 from mongoengine.connection import connect, disconnect, get_connection
@@ -6,19 +7,19 @@ from dto import ContactDTO, OrganizationDTO, PublicationDTO
 from dao import ContactDAO, OrganizationDAO, PublicationDAO
 from factory import DAOFactory
 
-class DatabaseInteractionTestSuite():
+class DatabaseInteractionTest(unittest.TestCase):
 
     mongodb_name = 'db_test'
 
     def __init__(self):
-        print "New DatabaseInteractionTestSuite running"
+        print "New DatabaseInteractionTest running"
         self.run_test()
 
     def init_database(self):
         
         disconnect()
         connect(self.mongodb_name)
-        print 'Creating mongo test-database ' + self.mongodb_name
+        print 'Creating mongo test-database: ' + self.mongodb_name
 
     def destroy_database(self):
         connection = get_connection()
@@ -34,9 +35,9 @@ class DatabaseInteractionTestSuite():
         my_contact.email = "jdegner0129@gmail.com"
         DAOFactory.GetInstance(ContactDAO).Create(my_contact)
         assert_contact=DAOFactory.GetInstance(ContactDAO).Find(my_contact.id)
-        assert assert_contact.first_name == "Jordan"
-        assert assert_contact.last_name == "Degner"
-        assert assert_contact.email == "jdegner0129@gmail.com"
+        self.assertTrue(assert_contact.first_name == "Jordan")
+        self.assertTrue(assert_contact.last_name == "Degner")
+        self.assertTrue(assert_contact.email == "jdegner0129@gmail.com")
         print 'ContactDAO tests passed'
 
         my_organization = OrganizationDTO()
@@ -48,9 +49,9 @@ class DatabaseInteractionTestSuite():
         my_organization.contacts = contacts
         DAOFactory.GetInstance(OrganizationDAO).Create(my_organization)
         assert_organization=DAOFactory.GetInstance(OrganizationDAO).Find(my_organization.id)
-        assert assert_organization.contacts == contacts
-        assert assert_organization.name == my_organization.name
-        assert assert_organization.organization_url == my_organization.organization_url
+        self.assertTrue(assert_organization.contacts == contacts)
+        self.assertTrue(assert_organization.name == my_organization.name)
+        self.assertTrue(assert_organization.organization_url == my_organization.organization_url)
         print 'OrganizationDAO tests passed'
 
         my_publication = PublicationDTO()
@@ -59,11 +60,11 @@ class DatabaseInteractionTestSuite():
         my_publication.publication_date = '2006-10-25 14:30:59'
         DAOFactory.GetInstance(PublicationDAO).Create(my_publication)
         assert_publication=DAOFactory.GetInstance(PublicationDAO).Find(my_publication.id)
-        assert assert_publication.title == my_publication.title
-        assert assert_publication.authors == my_publication.authors
+        self.assertTrue(assert_publication.title == my_publication.title)
+        self.assertTrue(assert_publication.authors == my_publication.authors)
         print 'PublicationDAO tests passed'
         self.destroy_database()
 
 if __name__ == '__main__':
-    my_suite = DatabaseInteractionTestSuite()
+    my_test = DatabaseInteractionTest()
     print 'DB interaction test complete'
