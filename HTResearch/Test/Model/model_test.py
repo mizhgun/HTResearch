37e07204cbcfc110f2711d8e93eb72047e1d19fe
Jordan Degner <jdegner0129@gmail.com>
2013-09-24 20:04:07 -0500

    Updated model_test to be functional with Python's unittest framework.

--- HTResearch/Test/Model/model_test.py
+++ HTResearch/Test/Model/model_test.py
@@ -13,21 +13,30 @@ import model
 
 class ModelTest(unittest.TestCase):
     def test_converter(self):
-        my_contact = model.Contact(first_name = "Jordan", last_name = "Degner")
+        my_contact = model.Contact(first_name = "Jordan", 
+                                   last_name = "Degner",
+                                   primary_phone = 4029813230,
+                                   secondary_phone = None,
+                                   email = "jdegner0129@gmail.com",
+                                   position = "Software Engineer")
 
         print 'Converting a contact to a DTO.'
         contact_dto = converter.DTOConverter.to_dto(dto.ContactDTO, my_contact)
 
         print 'Testing equality...'
-        assert my_contact.first_name == contact_dto.first_name
-        assert my_contact.last_name == contact_dto.last_name
+        for attr, value in my_contact.__dict__.iteritems():
+            self.assertEqual(getattr(my_contact, attr), getattr(contact_dto, attr), "{0} attribute not equal".format(attr))
 
         print 'Converting a DTO to a contact.'
-        my_contact_again = converter.DTOConverter.from_dto(model.Contact, contact_dto)
+        my_contact = converter.DTOConverter.from_dto(model.Contact, contact_dto)
 
         print 'Testing equality...'
-        assert my_contact_again.first_name == contact_dto.first_name
+        for attr, value in my_contact.__dict__.iteritems():
+            self.assertEqual(getattr(my_contact, attr), getattr(contact_dto, attr), "{0} attribute not equal".format(attr))
 
 if __name__ == '__main__':
-    unittest.main()
-    print 'Model testing complete.'
+    try:
+        unittest.main()
+    except SystemExit as inst:
+        if inst.args[0]:
+            raise
