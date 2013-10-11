1bfcc919906118c05c29943f7e011f2ce9895c50
Brian Yee <beeyee0401@gmail.com>
2013-10-10 19:39:37 -0500

    FIXED THE PATH THINGS

--- HTResearch/WebCrawler/WebCrawler/unittests/link_scraper_test.py
+++ HTResearch/WebCrawler/WebCrawler/unittests/link_scraper_test.py
@@ -9,6 +9,9 @@ class LinkScraperTest(BaseSpider):
     scraper = None
 
     def __init__(self, *args, **kwargs):
+        self.saved_path = os.getcwd()
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
         super(LinkScraperTest, self).__init__(*args, **kwargs)
         self.scraper = LinkScraper()
         if not os.path.exists("../Output/"):
@@ -18,6 +21,9 @@ class LinkScraperTest(BaseSpider):
                 os.remove("../Output/link_scraper_output.txt")
             except OSError:
                 pass
+
+    def __del__(self):
+        os.chdir(self.savedPath)
 
     def parse(self, response):
         links = self.scraper.parse(response)
