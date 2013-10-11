f0d9a32512146a757396b3d5d5f514dc27e4b195
Brian Yee <beeyee0401@gmail.com>
2013-10-10 21:06:29 -0500

    Deleted that line that screwed everything up

--- HTResearch/WebCrawler/WebCrawler/unittests/test_all_scrapers.py
+++ HTResearch/WebCrawler/WebCrawler/unittests/test_all_scrapers.py
@@ -29,7 +29,6 @@ class ScraperTests(unittest.TestCase):
 
     def test_email_scraper(self):
         # Runs the Test spider and pipes the printed output to "output"
-        os.chdir(os.path.join(os.pardir, os.pardir))
         p = subprocess.Popen('scrapy crawl email_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         output, error = p.communicate()
 
