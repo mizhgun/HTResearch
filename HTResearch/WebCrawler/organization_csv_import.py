a9a4558c957c016ecf3d6d4da987275e4efbcb91
Aubrey Thompson <aubreyannthompson@gmail.com>
2013-10-17 19:59:19 -0500

    Made a csv converter

---
+++ HTResearch/WebCrawler/organization_csv_import.py
@@ -0,0 +1,22 @@
+import csv
+from HTResearch.DataAccess.dto import *
+from HTResearch.DataAccess.dao import *
+from HTResearch.DataAccess.factory import DAOFactory
+from HTResearch.DataModel.model import *
+from HTResearch.DataModel.converter import DTOConverter
+
+
+with open('C:\Users\s-athomp44\Documents\organization.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        new_org = Organization(name=row[0],
+                               organization_url=row[1],
+                               address=row[2],
+                               phone_number=row[3],
+                               email=row[4])
+        org_dto = DTOConverter.to_dto(OrganizationDTO, new_org)
+        org_dao = DAOFactory.get_instance(OrganizationDAO)
+        org_dao.create_update(org_dto)
+
+
+
