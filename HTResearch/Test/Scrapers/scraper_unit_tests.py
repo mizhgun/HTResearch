c883880d2de10ae6d475fc98a99e01913d04e628
Paul Poulsen <ppoulsen21@gmail.com>
2013-10-24 17:05:54 -0500

    Email scraper test implemented

--- HTResearch/Test/Scrapers/scraper_unit_tests.py
+++ HTResearch/Test/Scrapers/scraper_unit_tests.py
@@ -56,31 +56,46 @@ class ScraperTests(unittest.TestCase):
                        {'city': "Mumbai", 'zip_code': '400064'},
                        {'city': "New Delhi", 'zip_code': '110019'},
                        {'city': "Mumbai", 'zip_code': '400052'},
-                       ]
+        ]
 
         for test in assert_list:
             self.assertIn(test, addresses, str(test) + " not found")
 
     def test_email_scraper(self):
-        # Runs the Test spider and pipes the printed output to "output"
-        p = subprocess.Popen('scrapy crawl email_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        output, error = p.communicate()
+        test_files = [
+            "httpnhrcnicinContactUshtm",
+            "httpbombayteenchallengeorg",
+            "httpwwwtisseduTopMenuBarcontactuslocation1",
+            "httpapneaaporgcontact",
+        ]
 
-        # Splits the results based on automatically added characters
-        emails = output.splitlines()
+        email_scraper = EmailScraper()
+        emails = []
+
+        for input_file in test_files:
+            response = file_to_response(input_file)
+            if response is not None:
+                ret = email_scraper.parse(response)
+                if isinstance(ret, type([])):
+                    emails = emails + ret
+                else:
+                    emails.append(ret)
+
+        print emails
 
         # Hardcoded results based on the sites that were crawled
-        assert_list = ["sgnhrc@nic.in",
-                       "covdnhrc@nic.in",
-                       "anilpradhanshilong@gmail.com",
-                       "snarayan1946@gmail.com",
-                       "tvarghese@bombayteenchallenge.org"]
+        assert_list = [{'email': "sgnhrc@nic.in"},
+                       {'email': "covdnhrc@nic.in"},
+                       {'email': "anilpradhanshilong@gmail.com"},
+                       {'email': "snarayan1946@gmail.com"},
+                       {'email': "tvarghese@bombayteenchallenge.org"}]
 
         for test in assert_list:
-            self.assertIn(test, emails, "Email " + test + " not found")
+            self.assertIn(test, emails, 'Email {0} not found'.format(str(test)))
 
     def test_link_scraper(self):
-        p = subprocess.Popen('scrapy crawl link_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('scrapy crawl link_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
         output, error = p.communicate()
         urls = output.splitlines()
         urls = [x.lower() for x in urls]
@@ -97,18 +112,20 @@ class ScraperTests(unittest.TestCase):
 
     def test_keyword_scraper(self):
         # Runs the Test spider and pipes the printed output to "output"
-        p = subprocess.Popen('scrapy crawl keyword_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('scrapy crawl keyword_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
         output, error = p.communicate()
         # Splits the results based on automatically added characters
         keywords = output.splitlines()
-        keywords = keywords[:len(keywords)-1]
+        keywords = keywords[:len(keywords) - 1]
 
         assert_list = ["nicolas", "cage"]
         for test in assert_list:
             self.assertIn(test, keywords, "Keyword " + test + " not found or frequent enough")
 
     def test_organization_scraper(self):
-        p = subprocess.Popen('scrapy crawl organization_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('scrapy crawl organization_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
         output, error = p.communicate()
         org = ast.literal_eval(output)
         assert_val = {
@@ -143,12 +160,17 @@ class ScraperTests(unittest.TestCase):
             self.assertIn(attr, org, 'Organization does not match - missing attribute \'' + attr + '\'')
             if type(assert_val[attr]) == list:
                 for item in assert_val[attr]:
-                    self.assertIn(item, org[attr], 'Organization does not match - attribute \'' + attr + '\' does not contain ' + str(item))
+                    self.assertIn(item, org[attr],
+                                  'Organization does not match - attribute \'' + attr + '\' does not contain ' + str(
+                                      item))
             else:
-                self.assertEqual(assert_val[attr], org[attr], 'Organization does not match - attribute \'' + attr + '\' is ' + str(org[attr]) + ', should be ' + str(assert_val[attr]))
+                self.assertEqual(assert_val[attr], org[attr],
+                                 'Organization does not match - attribute \'' + attr + '\' is ' + str(
+                                     org[attr]) + ', should be ' + str(assert_val[attr]))
 
     def test_org_type_scraper(self):
-        p = subprocess.Popen('scrapy crawl org_type_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('scrapy crawl org_type_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
         output, error = p.communicate()
         types = output.splitlines()
         types.pop()
@@ -159,15 +181,17 @@ class ScraperTests(unittest.TestCase):
 
     def test_phone_number_scraper(self):
         # Runs the Test spider and pipes the printed output to "output"
-        p = subprocess.Popen('scrapy crawl phone_number_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        p = subprocess.Popen('scrapy crawl phone_number_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
         output, error = p.communicate()
         # Splits the results based on automatically added characters
         numbers = output.splitlines()
-        numbers = numbers[:len(numbers)-1]
+        numbers = numbers[:len(numbers) - 1]
 
         assert_list = ["0402026070", "9435134726"]
         for test in assert_list:
             self.assertIn(test, numbers, "Phone number " + str(test) + " not found")
+
 
 if __name__ == '__main__':
     try:
