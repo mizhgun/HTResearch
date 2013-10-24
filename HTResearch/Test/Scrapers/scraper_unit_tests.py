70662330e21bbe8c0b67a6a8913a46178c1ed9aa
Paul Poulsen <ppoulsen21@gmail.com>
2013-10-24 17:25:53 -0500

    Organization scraper test implemented

--- HTResearch/Test/Scrapers/scraper_unit_tests.py
+++ HTResearch/Test/Scrapers/scraper_unit_tests.py
@@ -164,12 +164,26 @@ class ScraperTests(unittest.TestCase):
             self.assertIn(test, types, 'Type \'' + test + '\' not found')
 
     def test_organization_scraper(self):
-        p = subprocess.Popen('scrapy crawl organization_scraper_test', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                             shell=True)
-        output, error = p.communicate()
-        org = ast.literal_eval(output)
-        assert_val = {
-            'name': [], #'Bombay Teen Challenge', # not yet implemented
+        test_files = [
+            "httpbombayteenchallengeorg",
+        ]
+
+        organization_scraper = OrganizationScraper()
+        orgs = []
+
+        for input_file in test_files:
+            response = file_to_response(input_file)
+            if response is not None:
+                ret = organization_scraper.parse(response)
+                if isinstance(ret, type([])):
+                    orgs = orgs + ret
+                else:
+                    orgs.append(ret)
+
+        print orgs
+
+        assert_list = [{
+            'name': None, #'Bombay Teen Challenge', # not yet implemented
             'types': [
                 'religious',
                 'protection',
@@ -180,8 +194,8 @@ class ScraperTests(unittest.TestCase):
                 #{'phone_number': '912226042242'}, # Indian phone number for BTC not currently found by Indian phone number scraper
             ],
             'email': [
-                {'email': 'kkdevaraj@bombayteenchallenge.org'},
                 {'email': 'tvarghese@bombayteenchallenge.org'},
+                {'email': 'kkdevaraj@bombayteenchallenge.org'},
             ],
             'address': [
                 {'city': 'Mumbai', 'zip_code': '400052'},
@@ -190,23 +204,14 @@ class ScraperTests(unittest.TestCase):
             'contacts': [
                 # not yet implemented
             ],
-            'organization_url': 'https://bombayteenchallenge.org/',
+            'organization_url': 'http://bombayteenchallenge.org/',
             'partners': [
                 # not yet implemented
             ],
-        }
+        }]
 
-        for attr in assert_val.iterkeys():
-            self.assertIn(attr, org, 'Organization does not match - missing attribute \'' + attr + '\'')
-            if type(assert_val[attr]) == list:
-                for item in assert_val[attr]:
-                    self.assertIn(item, org[attr],
-                                  'Organization does not match - attribute \'' + attr + '\' does not contain ' + str(
-                                      item))
-            else:
-                self.assertEqual(assert_val[attr], org[attr],
-                                 'Organization does not match - attribute \'' + attr + '\' is ' + str(
-                                     org[attr]) + ', should be ' + str(assert_val[attr]))
+        for test in assert_list:
+            self.assertIn(test, orgs, 'Org \'' + str(test) + '\' not found')
 
     def test_phone_number_scraper(self):
         # Runs the Test spider and pipes the printed output to "output"
