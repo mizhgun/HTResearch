c1f7d8fdacb29891331983b60b5c5006b76d662f
Brian Yee <beeyee0401@gmail.com>
2013-10-25 18:33:46 -0500

    Removed old contact unit test

--- HTResearch/WebCrawler/app.py
+++ HTResearch/WebCrawler/app.py
@@ -2,13 +2,11 @@ from twisted.internet import reactor
 from scrapy.crawler import Crawler
 from scrapy import log
 from scrapy.utils.project import get_project_settings
-from WebCrawler.unittests.phone_number_scraper_test import PhoneNumberScraperTest
-from WebCrawler.unittests.keyword_scraper_test import KeywordScraperTest
-from WebCrawler.spiders import StopTraffickingSpider
+from WebCrawler.utility_spiders import DebugFileGenerator
 
 
 # TODO: define spiders we want to use
-spider = StopTraffickingSpider()
+spider = DebugFileGenerator(start_urls=["http://apneaap.org/about-us/our-board/"])
 settings = get_project_settings()
 crawler = Crawler(settings)
 crawler.configure()
