d2931bc246ea1d7ac157045c2603821b333622b5
Cameron Crockrom <ccrockrom@gmail.com>
2013-10-25 18:43:47 -0500

    More Work.

--- HTResearch/WebClient/WebClient/static/scripts/index.js
+++ HTResearch/WebClient/WebClient/static/scripts/index.js
@@ -1,8 +1,20 @@
 var searchResultsVisible = false;
 var map;
+var organizationImg = createImageHtml('static/images/office_building_icon.png');
 var initialLatLng = new google.maps.LatLng(21,78);
 var geocoder = new google.maps.Geocoder();
 var dummyAddress = "9-10-11 Nehru Place, New Delhi - 110019 India";
+var dummyContent = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h3 id="firstHeading" class="firstHeading">Save the Children India</h3>'+
+      '<div id="bodyContent">'+
+      organizationImg+
+      '<p>Tel: (+91) 11 4229 4900</p>'+
+      '<p>Email: info@savethechildren.in</p>'+
+      '<p>'+dummyAddress+'</p>'+
+      '</div>'+
+      '</div>';
 
 function initialize() {
 	var mapOptions = {
@@ -76,9 +88,24 @@ function plotOrganization(results, status) {
             map: map,
             position: results[0].geometry.location
         });
+
+        var infowindow = new google.maps.InfoWindow({
+		      content: dummyContent
+		  });
+
+        infowindow.open(map,marker);
+
+        google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.open(map,marker);
+		  });
+
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
+}
+
+function createImageHtml(imgUrl) {
+	return '<img src="'+imgUrl+'" class="col-md-3" style="margin-right: 10px;">';
 }
 
 google.maps.event.addDomListener(window, 'load', _.once(initialize));
