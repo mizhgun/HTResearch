15b02319cce78b775518a5240828ff17136eb928
Cameron Crockrom <ccrockrom@gmail.com>
2013-10-29 20:45:37 -0500

    Commiting quite a bit of work.

--- HTResearch/WebClient/WebClient/views.py
+++ HTResearch/WebClient/WebClient/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from HTResearch.WebClient.webclient.settings import GOOGLE_MAPS_API_KEY
+from HTResearch.DataModel.model import Organization
 
 def index(request):
     t = get_template('index_template.html')
@@ -10,5 +11,12 @@ def index(request):
 
 def organization_profile(request):
     t = get_template('organization_profile_template.html')
-    html = t.render(Context({}))
+    org = Organization(name="Bee Yee Foundation",
+                             address="1313 Yee Drive",
+                             phone_numbers=4029813230,
+                             email_key="beeyee@yeemail.com",
+                             organization_url="http://beeyee.com",
+                             facebook="facebook.com/yeeeeeee",
+                             twitter="BYeeeeeee")
+    html = t.render(Context({"organization": org}))
     return HttpResponse(html)
