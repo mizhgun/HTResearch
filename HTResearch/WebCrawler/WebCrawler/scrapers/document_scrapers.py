9743571e24d24482f8723f5cc740c2ac0a0c6380
Brian Yee <beeyee0401@gmail.com>
2013-11-07 16:03:06 -0600

    Made organization scraper require at least one word from a list of words that are common on the sites we want

--- HTResearch/WebCrawler/WebCrawler/scrapers/document_scrapers.py
+++ HTResearch/WebCrawler/WebCrawler/scrapers/document_scrapers.py
@@ -1,5 +1,7 @@
 from ..items import *
 from utility_scrapers import *
+import string
+import re
 
 
 class Contact:
@@ -9,6 +11,11 @@ class Contact:
 
 
 class OrganizationScraper():
+    def __init__(self):
+        self._required_words = ['trafficking', 'prostitution', 'sex trafficking', 'domestic', 'child labor',
+                                'child labour', 'slavery', 'trafficked']
+        self._punctuation = re.compile('[%s]' % re.escape(string.punctuation))
+
     _scrapers = {
         'name': [OrgNameScraper()],
         'address': [OrgAddressScraper()],
@@ -24,20 +31,34 @@ class OrganizationScraper():
 
     def parse(self, response):
         organization = ScrapedOrganization()
-        # Collect each field of organization model
-        for field in self._scrapers.iterkeys():
-            if field in self._multiple:
-                # Get multiple field (e.g. phone_number)
-                organization[field] = []
-                for scraper in self._scrapers[field]:
-                    organization[field] += scraper.parse(response)
-            else:
-                # Get single field (e.g. name)
-                results = self._scrapers[field][0].parse(response)
-                if results:
-                    organization[field] = results[0] if isinstance(results, type([])) else results
+        hxs = HtmlXPathSelector(response)
+        site_text = hxs.select('//html//text()').extract()
+        site_text = [element for element in site_text if element.strip() != '']
+        flag = False
+        for sentence in site_text:
+            for word in sentence.split():
+                word = self._punctuation.sub('', word)
+                if word in self._required_words:
+                    flag = True
+                    break
+            if flag:
+                break
+
+        if flag:
+            # Collect each field of organization model
+            for field in self._scrapers.iterkeys():
+                if field in self._multiple:
+                    # Get multiple field (e.g. phone_number)
+                    organization[field] = []
+                    for scraper in self._scrapers[field]:
+                        organization[field] += scraper.parse(response)
                 else:
-                    organization[field] = None
+                    # Get single field (e.g. name)
+                    results = self._scrapers[field][0].parse(response)
+                    if results:
+                        organization[field] = results[0] if isinstance(results, type([])) else results
+                    else:
+                        organization[field] = None
         return organization
 
 
