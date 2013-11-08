13058f4f7c4c4417c1c72562f07e487ec76c180e
Brian Yee <beeyee0401@gmail.com>
2013-11-07 21:04:13 -0600

    Combined all our stuff

--- HTResearch/WebClient/WebClient/views.py
+++ HTResearch/WebClient/WebClient/views.py
@@ -9,9 +9,23 @@ from HTResearch.DataAccess.dto import OrganizationDTO
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from mongoengine.fields import StringField, URLField
-from bson.json_util import dumps
 import string
+import json
+from mongoengine.fields import ObjectId
 import pdb
+
+
+class MongoJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+    def encode(self, o):
+        result = super(MongoJSONEncoder, self).encode(o)
+        if isinstance(o, ObjectId):
+            result = result[1:-1]
+        return result
 
 
 def index(request):
@@ -38,14 +52,8 @@ def search(request):
 
         organizations = org_dao.text_search(search_text, 10, 'name')
 
-        # Make each organization non-string attribute into valid JSON
-        fields_dict = OrganizationDTO._fields
-        string_types = (StringField, URLField)
-        json_fields = [key for key in fields_dict.iterkeys() if type(fields_dict[key]) not in string_types]
         for org in organizations:
-            # Find all non-string fields
-            for field in json_fields:
-                org[field] = dumps(org[field])
+            encode_org(org)
 
     params = {'organizations': organizations}
     return render_to_response('search_results.html', params)
@@ -56,12 +64,14 @@ def organization_profile(request):
     org_dao = OrganizationDAO()
 
     try:
-        org_lookup_key = int(string.split(uri,'/')[4])
+        org_lookup_key = string.split(uri, '/')[4]
         org = org_dao.find(id=org_lookup_key)
     except Exception as e:
         #If we ever hook up logging, this is where we would log the message
         print e.message
         return get_http_404_page()
+
+    encode_org(org)
 
     t = get_template('organization_profile_template.html')
     html = t.render(Context({"organization": org}))
@@ -72,3 +82,13 @@ def get_http_404_page():
     template = get_template('http_404.html')
     html = template.render(Context({}))
     return HttpResponseNotFound(html)
+
+# Encodes the fields to JSON
+def encode_org(org):
+    # Make each organization non-string attribute into valid JSON
+    fields_dict = OrganizationDTO._fields
+    string_types = (StringField, URLField)
+    json_fields = [key for key in fields_dict.iterkeys() if type(fields_dict[key]) not in string_types]
+    # Find all non-string fields
+    for field in json_fields:
+        org[field] = MongoJSONEncoder().encode(org[field])
