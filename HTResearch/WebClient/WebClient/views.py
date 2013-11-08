571e7fa10771a3156a92f4fb3ef2138566bae8ca
Brian Yee <beeyee0401@gmail.com>
2013-11-07 20:35:55 -0600

    Revert "combined Bee, Sam, and Cam's stuff"
    This reverts commit 7342a7e8f0b152157c83712ccac8c7dab514bf4f.

--- HTResearch/WebClient/WebClient/views.py
+++ HTResearch/WebClient/WebClient/views.py
@@ -9,23 +9,9 @@ from HTResearch.DataAccess.dto import OrganizationDTO
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from mongoengine.fields import StringField, URLField
-import json
-from mongoengine.fields import ObjectId
+from bson.json_util import dumps
 import string
 import pdb
-
-
-class MongoJSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
-
-    def encode(self, o):
-        result = super(MongoJSONEncoder, self).encode(o)
-        if isinstance(o, ObjectId):
-            result = result[1:-1]
-        return result
 
 
 def index(request):
@@ -38,6 +24,7 @@ def index(request):
 
 
 def search(request):
+
     if request.method == 'POST':
         search_text = request.POST['search_text']
     else:
@@ -51,9 +38,14 @@ def search(request):
 
         organizations = org_dao.text_search(search_text, 10, 'name')
 
+        # Make each organization non-string attribute into valid JSON
+        fields_dict = OrganizationDTO._fields
+        string_types = (StringField, URLField)
+        json_fields = [key for key in fields_dict.iterkeys() if type(fields_dict[key]) not in string_types]
         for org in organizations:
             # Find all non-string fields
-            encode_org(org)
+            for field in json_fields:
+                org[field] = dumps(org[field])
 
     params = {'organizations': organizations}
     return render_to_response('search_results.html', params)
@@ -64,28 +56,16 @@ def organization_profile(request):
     org_dao = OrganizationDAO()
 
     try:
-        org_lookup_key = string.split(uri, '/')[4]
+        org_lookup_key = int(string.split(uri,'/')[4])
         org = org_dao.find(id=org_lookup_key)
     except Exception as e:
         #If we ever hook up logging, this is where we would log the message
         print e.message
         return get_http_404_page()
 
-    #encode_org(org)
-
     t = get_template('organization_profile_template.html')
     html = t.render(Context({"organization": org}))
     return HttpResponse(html)
-
-
-def encode_org(org):
-     # Make each organization non-string attribute into valid JSON
-    fields_dict = OrganizationDTO._fields
-    string_types = (StringField, URLField)
-    json_fields = [key for key in fields_dict.iterkeys() if type(fields_dict[key]) not in string_types]
-    # Find all non-string fields
-    for field in json_fields:
-        org[field] = MongoJSONEncoder().encode(org[field])
 
 
 def get_http_404_page():
