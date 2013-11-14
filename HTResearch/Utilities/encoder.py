b3ffaa5271469e153822736876a3faaaa806d531
Paul Poulsen <ppoulsen21@gmail.com>
2013-11-14 14:46:58 -0600

    Refactored MongoJSONEncoder for better readability and less code repetition

--- HTResearch/Utilities/encoder.py
+++ HTResearch/Utilities/encoder.py
@@ -6,21 +6,22 @@ from UserString import MutableString
 
 
 class MongoJSONEncoder(json.JSONEncoder):
+    defined_types = (dict, list, tuple, str, unicode, int, long, float, bool, type(None))
+
     def default(self, o):
+        # If ObjectId, cast to string
         if isinstance(o, ObjectId):
             return str(o)
-        elif isinstance(o, list) and len(o) > 0 and all(
-                isinstance(x, (
-                    Contact, ContactDTO, Organization, OrganizationDTO, Publication, PublicationDTO, URLMetadata,
-                    URLMetadataDTO)) for x in o):
+        # If a list of any items that are custom objects, encode them ourselves
+        elif isinstance(o, list) and len(o) > 0 and any(not self._check_if_encodable(x) for x in o):
             new_list = []
             for item in list:
-                new_list.append(self.encode_ref(item))
+                new_list.append(self._encode_ref(item))
             return new_list
-        elif isinstance(o, (
-            Contact, ContactDTO, Organization, OrganizationDTO, Publication, PublicationDTO, URLMetadata,
-            URLMetadataDTO)):
-            return self.encode_ref(o)
+        # If a custom object, encode ourselves
+        elif not self._check_if_encodable(o):
+            return self._encode_ref(o)
+        # Otherwise, return
         return json.JSONEncoder.default(self, o)
 
     def encode(self, o):
@@ -29,10 +30,15 @@ class MongoJSONEncoder(json.JSONEncoder):
             result = result[1:-1]
         return result
 
-    def encode_ref(self, o):
+    def _encode_ref(self, o):
         # Convert objects to a dictionary of their representation
         d = {'__class__': o.__class__.__name__,
              '__module__': o.__module__,
         }
         d.update(o.__dict__)
         return d
+
+    def _check_if_encodable(self, o):
+        if isinstance(o, MongoJSONEncoder.defined_types):
+            return True
+        return False
