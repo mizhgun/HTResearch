30864db2dba6f936facb8242fc5d097630842c5e
Paul Poulsen <ppoulsen21@gmail.com>
2013-11-13 19:07:03 -0600

    Organizations with partners can now be encoded to JSON. Should hold for all reference objects. Gotta be careful about cyclical references.

--- HTResearch/Utilities/encoder.py
+++ HTResearch/Utilities/encoder.py
@@ -1,11 +1,26 @@
 import json
-from mongoengine.fields import ObjectId
+from mongoengine.fields import ObjectId, StringField, URLField
+from HTResearch.DataModel.model import *
+from HTResearch.DataAccess.dto import *
+from UserString import MutableString
 
 
 class MongoJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
+        elif isinstance(o, list) and len(o) > 0 and all(
+                isinstance(x, (
+                    Contact, ContactDTO, Organization, OrganizationDTO, Publication, PublicationDTO, URLMetadata,
+                    URLMetadataDTO)) for x in o):
+            new_list = []
+            for item in list:
+                new_list.append(self.encode_ref(item))
+            return new_list
+        elif isinstance(o, (
+            Contact, ContactDTO, Organization, OrganizationDTO, Publication, PublicationDTO, URLMetadata,
+            URLMetadataDTO)):
+            return self.encode_ref(o)
         return json.JSONEncoder.default(self, o)
 
     def encode(self, o):
@@ -13,3 +28,11 @@ class MongoJSONEncoder(json.JSONEncoder):
         if isinstance(o, ObjectId):
             result = result[1:-1]
         return result
+
+    def encode_ref(self, o):
+        # Convert objects to a dictionary of their representation
+        d = {'__class__': o.__class__.__name__,
+             '__module__': o.__module__,
+        }
+        d.update(o.__dict__)
+        return d
