5c0434f9694aa151bbf381e497ed43d8a09ee7e5
Cameron Crockrom <ccrockrom@gmail.com>
2013-11-14 14:44:13 -0600

    Utilized Django's URL scheming for efficiency.

--- HTResearch/WebClient/WebClient/views.py
+++ HTResearch/WebClient/WebClient/views.py
@@ -44,13 +44,11 @@ def search(request):
     return render_to_response('search_results.html', params)
 
 
-def organization_profile(request):
-    uri = request.build_absolute_uri()
+def organization_profile(request, org_id):
     org_dao = OrganizationDAO()
 
     try:
-        org_lookup_key = string.split(uri, '/')[4]
-        org = org_dao.find(id=org_lookup_key)
+        org = org_dao.find(id=org_id)
     except Exception as e:
         #If we ever hook up logging, this is where we would log the message
         print e.message
@@ -61,13 +59,11 @@ def organization_profile(request):
     return HttpResponse(html)
 
 
-def contact_profile(request):
-    uri = request.build_absolute_uri()
+def contact_profile(request, contact_id):
     contact_dao = ContactDAO()
 
     try:
-        contact_lookup_key = string.split(uri, '/')[4]
-        contact = contact_dao.find(id=contact_lookup_key)
+        contact = contact_dao.find(id=contact_id)
     except Exception as e:
         #If we ever hook up logging, this is where we would log the message
         print e.message
@@ -76,7 +72,6 @@ def contact_profile(request):
     org_urls = []
     for org in contact.organizations:
         org_urls.append("/organization/"+org.id)
-
 
     #Generates a 2d list
     contact.organizations = zip(contact.organizations, org_urls)
