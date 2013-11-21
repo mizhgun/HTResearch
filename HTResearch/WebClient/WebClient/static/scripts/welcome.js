5c0d9272a33286206ec458d469c771af6d4aa1ca
Cameron Crockrom <ccrockrom@gmail.com>
2013-11-20 21:09:42 -0600

    Adding a lot of work. Up to 3 stages of the tutorial are functional.

--- HTResearch/WebClient/WebClient/static/scripts/welcome.js
+++ HTResearch/WebClient/WebClient/static/scripts/welcome.js
@@ -1,12 +1,19 @@
 //This function is called from index.js
 function initiateTutorial() {
 	/*var visited=getCookie("htresearch");
+	if(visited)
+		return;
 
-	if (!visited){
-	   var expire=new Date();
-	   expire=new Date(expire.getTime()+7776000000);
-	   document.cookie="htresearch=here; expires="+expire;
+   var expire=new Date();
+   expire=new Date(expire.getTime()+7776000000);
+   document.cookie="htresearch=here; expires="+expire;
 	}*/
+
+	$('#welcome-1').easyModal({
+		closeButtonClass: '.welcome-close',
+		autoOpen: true,
+		onClose: initWelcome2
+	});
 }
 
 function getCookie(name) {
@@ -22,4 +29,33 @@ function getCookie(name) {
 	  if (i==0) break;
 	}
 	return null;
+}
+
+function initWelcome2() {
+	$('#welcome-2').easyModal({
+		closeButtonClass: '.welcome-close',
+		onClose: initWelcome3,
+		overlayOpacity: 0.2,
+		top: 50
+	});
+
+	$('#welcome-2').trigger('openModal');
+}
+
+function initWelcome3() {
+	$('#welcome-3').easyModal({
+		closeButtonClass: '.welcome-close',
+		overlayOpacity: 0.2,
+		top: window.innerHeight - 150
+	});
+
+	//The modal coordinates aren't properly constructed the first time for some reason
+	//So the modal must be opened, closed, then opened again
+	$('#welcome-3').trigger('openModal');
+	$('#welcome-3').trigger('closeModal');
+	$('#welcome-3').trigger('openModal');
+}
+
+function initWelcome4() {
+
 }
