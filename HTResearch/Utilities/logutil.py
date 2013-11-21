1f7009a8e7b2e881cef6ff475594600e8f9a4f21
Jordan Degner <jdegner0129@gmail.com>
2013-11-20 19:37:58 -0600

    Added more precision to the logging formatter, and converted time to UTC.

--- HTResearch/Utilities/logutil.py
+++ HTResearch/Utilities/logutil.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 
 from types import Singleton
 from data_structures import enum
@@ -32,11 +33,12 @@ class LoggingUtility(object):
         if not os.path.exists(logdir):
             os.mkdir(logdir)
 
+        logging.Formatter.converter = time.gmtime
         logging.basicConfig(level=logging.DEBUG,
-                            format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                            datefmt='%m-%d %H:%M',
+                            format='[%(asctime)s %(levelname)s] %(name)s::%(funcName)s - %(message)s',
+                            datefmt='%x %X %Z',
                             filename=logfile,
-                            filemode='w+')
+                            filemode='a+')
 
     def get_logger(self, section, name):
         section_name = LoggingSection.reverse_mapping[section].lower()
