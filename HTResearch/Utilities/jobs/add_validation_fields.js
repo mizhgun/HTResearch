d728423ecb168545e160e6e84abf3bb3fac003c5
Jordan Degner <jdegner0129@gmail.com>
2014-01-23 20:46:22 -0600

    Added migration script.

---
+++ HTResearch/Utilities/jobs/add_validation_fields.js
@@ -0,0 +1,5 @@
+db.contact_d_t_o.update({}, { $set: { 'v': true, 'lu': new Date() } }, { upsert: false, multi: true });
+db.organization_d_t_o.update({}, { $set: { 'v': true, 'lu': new Date() } }, { upsert: false, multi: true });
+db.publication_d_t_o.update({}, { $set: { 'v': true, 'lu': new Date() } }, { upsert: false, multi: true });
+db.u_r_l_metadata_d_t_o.update({}, { $set: { 'v': true, 'lu': new Date() } }, { upsert: false, multi: true });
+db.user_dto.update({}, { $set: { 'lu': new Date() } }, { upsert: false, multi: true });
