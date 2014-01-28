c7022a4f551f7d43caadc23379c85fbed970eec7
Sam Adams <samadams3@me.com>
2014-01-28 15:31:23 -0600

    merge R4

---
+++ HTResearch/Utilities/jobs/change_keyword_field_to_string.js
@@ -0,0 +1,14 @@
+db = connect('unlhtprod.cloudapp.net:27017/ht');
+
+var c = db.organization_d_t_o.find();
+while(c.hasNext()) {
+	var org = c.next();
+	var keywordDict = org.ks;
+	var keywordList = [];
+	for(key in keywordDict) {
+		keywordList.push(key);
+	}
+	var keywordString = keywordList.join(' ');
+	org.ks = keywordString;
+	db.organization_d_t_o.save(org);
+}
