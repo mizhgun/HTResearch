d07e33674035acb8c560c94539901040369e4a50
Sam Adams <samadams3@me.com>
2014-02-06 20:52:02 -0600

    added view for publication search

---
+++ HTResearch/WebClient/WebClient/views/publication_views.py
@@ -0,0 +1,32 @@
+from urlparse import urlparse
+import json
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from springpython.context import ApplicationContext
+
+from HTResearch.Utilities.context import DAOContext
+from HTResearch.Utilities.logutil import LoggingSection, get_logger
+
+from HTResearch.Utilities.encoder import MongoJSONEncoder
+
+
+logger = get_logger(LoggingSection.CLIENT, __name__)
+ctx = ApplicationContext(DAOContext())
+
+
+def search_publications(request):
+    if request.method == 'GET':
+        search_text = request.GET['search_text']
+    else:
+        search_text = ''
+
+    publications = []
+
+    if search_text:
+        pub_dao = ctx.get_object('PublicationDAO')
+        publications = pub_dao.findmany(search=search_text, num_elements=10, sort_fields=['name'])
+
+    data = {'results': publications}
+    return HttpResponse(json.dumps(data, cls=MongoJSONEncoder), content_type='application/json')
