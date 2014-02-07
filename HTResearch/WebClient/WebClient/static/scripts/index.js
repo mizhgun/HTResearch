d3dcc89c34c45505c5f238579f5e894a6d310bb0
Jordan Degner <jdegner0129@gmail.com>
2014-02-06 20:57:10 -0600

    Updated function chain to be a little more pretty.

--- HTResearch/WebClient/WebClient/static/scripts/index.js
+++ HTResearch/WebClient/WebClient/static/scripts/index.js
@@ -292,7 +292,7 @@ function plotMarker(data) {
 
         $(document).bind("mousedown", function(e){
             //TODO: Find a not janky way - Marcus
-            $('#map-modal').parent().parent().parent().attr('id', 'map-modal-parent');
+            $('#map-modal').parents().eq(2).attr('id', 'map-modal-parent');
             if((!$(e.target).parents('#map-modal-parent').size() || e.target.id == "map-modal-parent")) {
                 closeAllInfowindows();
             }
