9dd5e51f35303e1c2544b93075d8d2d76f4ee0e8
Brian Yee <beeyee0401@gmail.com>
2014-02-18 16:40:08 -0600

    Used font awesome icons

--- HTResearch/WebClient/WebClient/static/scripts/media-sharing.js
+++ HTResearch/WebClient/WebClient/static/scripts/media-sharing.js
@@ -1,28 +1,12 @@
 $(document).ready(function() {
   var ele = document.getElementsByClassName('share');
   var ids = [];
-  var mappings = {
-      facebook: "Facebook",
-      twitter: "Twitter",
-      gplus: "Google+",
-      linkedin: "LinkedIn"
-  }
 
   for (var i = 0; i < ele.length; i++) {
     ids.push(ele[i].id);
   }
 
   for (var i = 0; i < ele.length; i++){
-    // Add image
-    var img = document.createElement("img")
-    img.src = "../../static/images/" + ids[i] + ".png";
-    img.alt = "Share on " + mappings[ ids[i] ];
-    img.title = "Share on " + mappings[ ids[i] ];
-    ele[i].appendChild(img);
-
-    // Add the name
-    ele[i].innerHTML += " " + mappings[ ids[i] ];
-
     // Add href
     switch (ids[i]){
         case "facebook":
