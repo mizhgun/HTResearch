989099d097c5a5b5739d9de3cfde474cfaf24170
Brian Yee <beeyee0401@gmail.com>
2014-02-18 20:00:57 -0600

    changed size of windows

--- HTResearch/WebClient/WebClient/static/scripts/index/media-sharing.js
+++ HTResearch/WebClient/WebClient/static/scripts/index/media-sharing.js
@@ -3,6 +3,7 @@ define(['jquery', 'underscore'], function($, _) {
 
     function initialize() {
         var elements = $('.share');
+        var height, width;
 
         _.each(elements, function(ele){
             // Add href
@@ -10,16 +11,24 @@ define(['jquery', 'underscore'], function($, _) {
                 case "facebook":
                     ele.href = 'http://www.facebook.com/sharer.php?s=100&p[title]=Anti-Human%20Trafficking%20Map&p[summary]=' +
                         'A%20map%20of%20anti-human%20trafficking%20efforts&p[url]=' + SITE_URL;
+                    height = 450;
+                    width = 420;
                     break;
                 case "twitter":
                     ele.href = 'https://twitter.com/intent/tweet?url=' + SITE_URL;
+                    height = 550;
+                    width = 420;
                     break;
                 case "gplus":
                     ele.href = 'https://plus.google.com/share?url=' + SITE_URL;
+                    height = 500;
+                    width = 450;
                     break;
                 case "linkedin":
                     ele.href = 'http://www.linkedin.com/shareArticle?mini=true&url=' + SITE_URL + '&title=' +
                         'Anti-Human%20Trafficking%20Map';
+                    height = 450;
+                    width = 450;
                     break;
             }
         });
@@ -28,7 +37,7 @@ define(['jquery', 'underscore'], function($, _) {
         elements.click(function(e) {
             e.preventDefault();
             e.stopPropagation();
-            window.open(this.href, '_blank', 'status=1,height=550,width=420');
+            window.open(this.href, '_blank', 'status=1,height=' + height + ',width=' + width);
         });
     }
 
