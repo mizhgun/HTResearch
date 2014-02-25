5fc537d915e39237b77b73b8d55fdfe4a9e790e7
Camtendo <ccrockrom@gmail.com>
2014-02-25 17:41:39 -0600

    Initial commit. Testing writing sessions straight to Mongo.

--- HTResearch/WebClient/WebClient/settings.py
+++ HTResearch/WebClient/WebClient/settings.py
@@ -1,4 +1,5 @@
 import os.path
+import mongoengine
 # Django settings for HTResearch.WebClient project.
 
 DEBUG = True
@@ -12,14 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '', # Or path to database file if using sqlite3.
-        'USER': '', # Not used with sqlite3.
-        'PASSWORD': '', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.dummy',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     }
 }
+
+#Hardcoded for proof of concept first
+mongoengine.connect(db='ht', host='localhost', port=27017)
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -116,7 +115,8 @@ CACHES = {
     }
 }
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+#SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'mongoengine.django.sessions'
 
 SESSION_SAVE_EVERY_REQUEST = True
 
