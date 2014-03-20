938ad82ef49092aa2281220194637632f487eff8
Jordan Degner <jdegner0129@gmail.com>
2014-03-19 20:11:20 -0500

    Added documentation to init functions.

--- HTResearch/DataModel/model.py
+++ HTResearch/DataModel/model.py
@@ -14,6 +14,21 @@ class Contact(object):
                  phones=[], email=None, organization=None,
                  publications=[], position=None, valid=True,
                  updated_by=None, content_weight=None):
+        """
+        Constructs a new Contact instance.
+
+        Arguments:
+            first_name (string): The contact's first name.
+            last_name (string): The contact's last name.
+            phones (string[]): The contact's phone numbers.
+            email (string): The contact's email.
+            organization (Organization): The contact's affiliated organization.
+            publications (Publication[]): The contact's publications.
+            valid (boolean): The validity of the contact.
+            last_updated (DateTime): The date the contact was last updated.
+            updated_by (ObjectId): The user that last updated the contact.
+            content_weight (float): The weight of the contact's content.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.phones = phones
@@ -38,6 +53,30 @@ class Organization(object):
                  keywords=None, valid=True, updated_by=None,
                  page_rank_info=None, page_rank=None, page_rank_weight=None,
                  content_weight=None, combined_weight=None):
+        """
+        Constructs a new Organization instance.
+
+        Arguments:
+            name (string): The name of the organization.
+            address (string): The address of the organization.
+            types (OrgTypeEnum[]): The types of the organization.
+            phone_numbers (string[]): The phone numbers for the organization.
+            email_key (string): The primary email for the organization.
+            emails (string[]): The emails for the organization.
+            contacts (Contact[]): The contacts for the organization.
+            organization_url (string): The URL for the organization's website.
+            partners (Organization[]): The partner organizations.
+            facebook (string): The Facebook URL for the organization.
+            twitter (string): The Twitter URL for the organization.
+            keywords (string[]): The list of keywords.
+            valid: Whether or not the organization is valid.
+            updated_by (ObjectId): The object id of the last user to update the organization.
+            page_rank_info (PageRankInfo): The organization's PageRank information.
+            page_rank (int): The PageRank value for the organization.
+            page_rank_weight (float): A weight from 0 - 1 for the organization's Page Rank.
+            content_weight (float): A weight from 0 - 1 for the organization's content.
+            combined_weight (float): The combined weight of content and PageRank.
+        """
         self.name = name
         self.address = address
         self.types = types
@@ -65,6 +104,12 @@ class PageRankInfo(object):
 
     def __init__(self, total_with_self=0, total=0,
                  references=[]):
+        """
+        Constructs a new PageRankInfo instance.
+
+        Arguments:
+            TODO: put arguments here
+        """
         self.total_with_self = total_with_self
         self.total = total
         self.references = references
@@ -75,6 +120,12 @@ class PageRankVector(object):
 
     def __init__(self, org_domain=None, count=0,
                  pages=[]):
+        """
+        Constructs a new PageRankVector instance.
+
+        Arguments:
+            TODO: put arguments here
+        """
         self.org_domain = org_domain
         self.count = count
         self.pages = pages
@@ -87,6 +138,19 @@ class Publication(object):
                  publisher=None, publication_date=datetime.utcnow(),
                  types=[], content_url=None, valid=True,
                  updated_by=None):
+        """
+        Constructs a new Publication instance.
+
+        Arguments:
+            title (string): The publication title.
+            authors (Contact[]): The list of authors.
+            publisher (string): The publisher of the publication.
+            publication_date (DateTime): The date of publication.
+            types (OrgTypeEnum[]): The types associated with the publication.
+            content_url (string): The URL for the publication.
+            valid (boolean): The validity of the publication.
+            updated_by (ObjectId): The object id of the publication's last updater.
+        """
         self.title = title
         self.authors = authors
         self.publisher = publisher
@@ -99,9 +163,16 @@ class Publication(object):
 
 
 class UrlCountPair(object):
-    """A Model class for pairing Source URLs and the Number of References to some Page"""
+    """A Model class for pairing source URLs and the number of references to some page."""
 
     def __init__(self, url=None, count=0):
+        """
+        Constructs a new UrlCountPair object.
+
+        Arguments:
+            url (string): The URL for this pairing.
+            count (int): The number of instances of this URL.
+        """
         self.url = url
         self.count = count
 
@@ -112,6 +183,17 @@ class URLMetadata(object):
     def __init__(self, url=None, domain=None, last_visited=datetime.utcnow(),
                  score=None, update_freq=None,
                  checksum=None):
+        """
+        Constructs a new URLMetadata object.
+
+        Arguments:
+            url (string): The URL this metadata is for.
+            domain (string): The domain for the URL.
+            last_visited (DateTime): The last visitation date for this URL.
+            score (int): The score for this URL.
+            update_freq (long): The frequency of updates to this page.
+            checksum (string): The checksum for the URL's content.
+        """
         self.url = url
         self.domain = domain
         self.last_visited = last_visited
@@ -128,6 +210,20 @@ class User(object):
                  email=None, password=None, background=None,
                  account_type=None, org_type=None,
                  organization=None, content_weight=None):
+        """
+        Constructs a new User object.
+
+        Arguments:
+            first_name (string): The user's first name.
+            last_name (string): The user's last name.
+            email (string): The user's email.
+            password (string): The user's password.
+            background (string): The user's professional background.
+            account_type (AccountType): The user's account type.
+            org_type (OrgTypeEnum): The user's organization type.
+            organization (Organization): The user's affiliated organization.
+            content_weight (float): The 0 - 1 ranking of the user content.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
