4348c5c5f53db05e4362fe298cc057a82d5a83cf
Marcus Pasell <rickyrombo@gmail.com>
2014-04-04 18:40:44 -0500

    Fixed a bug and added logging

--- HTResearch/Utilities/jobs/cascading_removal_for_unlinked_references.js
+++ HTResearch/Utilities/jobs/cascading_removal_for_unlinked_references.js
@@ -7,7 +7,10 @@ while(orgs.hasNext()) {
         var contact = db.contact_d_t_o.find({_id: c_id});
         if (contact.toArray().length === 0) {
             //Delete the references to contacts that no longer exist
-            contact_ids.splice(key, 1);
+            var removed = contact_ids.splice(key, 1);
+            print('Broken org->contact link found');
+            printjsononeline(org._id);
+            printjsononeline(removed);
         }
     });
     org.cs = contact_ids;
@@ -20,6 +23,9 @@ while(contacts.hasNext()){
     var contact = contacts.next();
     var org = db.organization_d_t_o.find({_id: contact.o});
     if (org.toArray().length === 0) {
+        print('Broken contact->org link found');
+        printjsononeline(contact._id);
+        printjsononeline(contact.o);
         //Delete the references to organizations that no longer exist
         db.contact_d_t_o.update({ '_id': contact._id},
         { $unset: {
@@ -29,16 +35,19 @@ while(contacts.hasNext()){
 }
 
 //Search all partner organizations linked in each organization
-var host_orgs = db.organization_d_t_o.find({ps: {$not: {$size: 0}}});
+var host_orgs = db.organization_d_t_o.find({ps: {$exists: {$not: {$size: 0}}}});
 while(host_orgs.hasNext()) {
     var host_org = host_orgs.next();
     var partners = host_org.ps;
     partners.forEach(function(partner, key){
         if (db.organization_d_t_o.find({_id: partner._id}).toArray().length === 0) {
             //Delete the references to organizations that no longer exist
-            partners.splice(key, 1);
+            var removed = partners.splice(key, 1);
+            print('Broken org->partner org link found');
+            printjsononeline(org._id);
+            printjsononeline(removed);
         }
     });
     host_org.ps = partners;
-    db.organization_d_t_o.save(org);
+    db.organization_d_t_o.save(host_org);
 }
