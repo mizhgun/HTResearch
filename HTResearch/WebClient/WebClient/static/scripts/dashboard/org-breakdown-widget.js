f19c69bdd29a13baa40684b66e60c31aec461310
Sam Adams <samadams3@me.com>
2014-04-04 15:26:53 -0500

    consolidated org breakdown demo, partner map demo, and counter into dashboard.js file

--- HTResearch/WebClient/WebClient/static/scripts/dashboard/org-breakdown-widget.js
+++
@@ -1,8 +0,0 @@
-require(['datavis/org-breakdown', 'bootstrap'], function(OrgBreakdown) {
-    var options = {
-        width: 350,
-        height: 350,
-        radius: 150
-    };
-    OrgBreakdown.initialize('#orgs-by-region', '#orgs-by-type', '#orgs-by-members', options);
-});
