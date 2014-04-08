911664662508a4aab6e5202f585b3d77c386fd5f
Brian Yee <beeyee0401@gmail.com>
2014-04-08 13:57:24 -0500

    Removed closure

--- HTResearch/WebClient/WebClient/static/scripts/shared/about-us.js
+++ HTResearch/WebClient/WebClient/static/scripts/shared/about-us.js
@@ -5,8 +5,6 @@
 require(['shared/analytics', 'jquery', 'bootstrap'], function(Analytics, $) {
     'use strict';
 
-    $(function (){
-        Analytics.startTracking();
-        $('#about-us-nav').addClass('active');
-    });
+    Analytics.startTracking();
+    $('#about-us-nav').addClass('active');
 });
