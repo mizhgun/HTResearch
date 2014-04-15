26561d837ca726d528b19933c2d59e98422c6f4d
Camtendo <ccrockrom@gmail.com>
2014-04-15 17:50:36 -0500

    More robust.

--- HTResearch/WebClient/WebClient/static/scripts/shared/welcome.js
+++ HTResearch/WebClient/WebClient/static/scripts/shared/welcome.js
@@ -7,7 +7,7 @@ require(['shared/analytics', 'jquery'], function(Analytics, $) {
 
     Analytics.startTracking();
 
-    $('a').click(function () {
+    $('#create-cookie').click(function () {
         var expire = new Date();
         expire = new Date(expire.getTime() + 7776000000);
         document.cookie = "htresearchv2=amaterasu; path=/; expires=" + expire;
