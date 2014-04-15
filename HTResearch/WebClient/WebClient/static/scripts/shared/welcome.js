a5e2f9ca495d07752dbc33a56ede4e77fdc7f5e0
Camtendo <ccrockrom@gmail.com>
2014-04-15 17:43:48 -0500

    Edited click event listener.

--- HTResearch/WebClient/WebClient/static/scripts/shared/welcome.js
+++ HTResearch/WebClient/WebClient/static/scripts/shared/welcome.js
@@ -7,7 +7,7 @@ require(['shared/analytics', 'jquery'], function(Analytics, $) {
 
     Analytics.startTracking();
 
-    $('.btn').click(function () {
+    $('a').click(function () {
         var expire = new Date();
         expire = new Date(expire.getTime() + 7776000000);
         document.cookie = "htresearchv2=amaterasu; path=/; expires=" + expire;
