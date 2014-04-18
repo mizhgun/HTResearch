2e776f34cb525e12b11d6f5231f611f761db2cc8
Paul Poulsen <ppoulsen21@gmail.com>
2014-04-17 19:43:07 -0500

    Fixed them scripts.

--- HTResearch/Utilities/jobs/cascading_removal_for_unlinked_references.js
+++ HTResearch/Utilities/jobs/cascading_removal_for_unlinked_references.js
@@ -3,18 +3,19 @@ var orgs = db.organization_d_t_o.find({cs: {$not: {$size: 0}}});
 while(orgs.hasNext()) {
     var org = orgs.next();
     var contact_ids = org.cs;
-    contact_ids.forEach(function(c_id, key) {
-        var contact = db.contact_d_t_o.find({_id: c_id});
+    for (var i = contact_ids.length - 1; i >= 0; i -= 1) {
+        var id = contact_ids[i];
+        var contact = db.contact_d_t_o.find({_id: id});
         if (!contact.hasNext()) {
             //Delete the references to contacts that no longer exist
-            var removed = org.cs.splice(key, 1);
+            var removed = org.cs.splice(i, 1);
             print('Broken org->contact link found');
             print('Org: ');
             printjsononeline(org._id);
             print('Contact: ');
             printjsononeline(removed);
         }
-    });
+    }
     db.organization_d_t_o.save(org);
 }
 
@@ -37,21 +38,42 @@ while(contacts.hasNext()){
     }
 }
 
+//Search for all organizations linked in each user
+var users = db.user_d_t_o.find({o: {$exists:true}});
+while(users.hasNext()){
+    var user = users.next();
+    var org = db.organization_d_t_o.find({_id: user.o});
+    if (!org.hasNext()) {
+        print('Broken user->org link found');
+        print('User: ');
+        printjsononeline(user._id);
+        print('Org: ');
+        printjsononeline(user.o);
+        //Delete the references to organizations that no longer exist
+        db.user_d_t_o.update({ '_id': user._id},
+        { $unset: {
+            "o": ""
+        }});
+    }
+}
+
 //Search all partner organizations linked in each organization
 var host_orgs = db.organization_d_t_o.find({ps: {$exists: {$not: {$size: 0}}}});
 while(host_orgs.hasNext()) {
     var host_org = host_orgs.next();
     var partners = host_org.ps;
-    partners.forEach(function(partner, key){
-        if (db.organization_d_t_o.find({_id: partner._id}).toArray().length === 0) {
+    for (var i = partners.length - 1; i >= 0; i -= 1) {
+        var id = partners[i];
+        var partner = db.organization_d_t_o.find({_id: id});
+        if (!partner.hasNext()) {
             //Delete the references to organizations that no longer exist
-            var removed = host_org.ps.splice(key, 1);
+            var removed = host_org.ps.splice(i, 1);
             print('Broken org->partner org link found');
             print('Org:');
-            printjsononeline(org._id);
+            printjsononeline(host_org._id);
             print('Partner Org:');
             printjsononeline(removed);
         }
-    });
+    }
     db.organization_d_t_o.save(host_org);
 }
